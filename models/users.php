99c59ab606c7794020e87468b904bb26b24f3c69
Ines Estiveira <ines-estiveira@hotmail.com>
2021-07-03 17:33:17 +0100

    created function to insert info into DB

--- models/users.php
+++ models/users.php
@@ -3,6 +3,7 @@
 require("base.php");
 
 class Users extends Base {
+
     public function getDetail($username) {
         $query = $this->db->prepare("
         SELECT user_id, username, password, email, phone, full_name, gender
@@ -15,5 +16,28 @@ class Users extends Base {
         return $query->fetch( PDO::FETCH_ASSOC );
     }
 
-    
+    public function create($user) {
+        $userExists = $this->getDetail( $user["username"] );
+
+        if(!empty($userExists)) {
+            return 0;
+        }
+        
+        $query = $this->db->prepare("
+            INSERT INTO users
+            (username, password, email, phone, full_name, gender)
+            VALUES(?, ?, ?, ?, ?, ?)
+        ");
+
+        $query->execute([
+            $user["username"],
+            password_hash($user["password"], PASSWORD_DEFAULT),
+            $user["email"],
+            $user["phone"],
+            $user["full_name"],
+            $user["gender"]
+        ]);
+
+        return $this->db->lastInsertId();
+    }
 }
